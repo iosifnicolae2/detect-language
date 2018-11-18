dfd58f7b961698d1719e256dc7b4d9b12ed47482
iosifnicolae2 <iosif@vitaminsoftware.com>
2018-11-19 00:37:29 +0200

    Create start.sh

---
+++ start.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+sudo apt-get install python-dev python-pip
+
+pip3 install -U virtualenv
+
+virtualenv -p python3 env-dlkeras
+source env-dlkeras/bin/activate
+pip install --upgrade -r requirements.txt
+
+python python_script.py
